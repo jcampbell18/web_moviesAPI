2e475b01f70aa6e6e070026e375b7629e85d0513
Jason Campbell <43692594+jcampbell18@users.noreply.github.com>
2019-04-29 09:52:44 -0700

    Updated scripts.js
    updated url to new movie list from Val

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,9 @@ $('.currentYear').text( (new Date).getFullYear() );
 **********************************/ 
 // The API feed
 //const url = 'https://gist.githubusercontent.com/jcampbell18/8b4cab8b820e61a7acc86e112af0e12c/raw/aa024380fbef9706b188a02589cecb13ebd0ff11/movies-v2.json';
-const url = 'https://gist.githubusercontent.com/jcampbell18/c8d7476b3367ee739ecb7b7757cc135b/raw/9a66726b778fbc84b5d2cd88bef3dd303f063170/movies.json';
+//const url = 'https://gist.githubusercontent.com/jcampbell18/c8d7476b3367ee739ecb7b7757cc135b/raw/9a66726b778fbc84b5d2cd88bef3dd303f063170/movies.json';
+const url = 'https://gist.githubusercontent.com/jcampbell18/0e1da3a85ff8698092a90357d39c0102/raw/000503a6cc57896feb008c47b20a9d8e3cfa77db/combined-movies.json';
+
 
 // Do stuff with returned data
 let doStuff = function(data) {
